c061917b75442c34d12ae21723a6db7e5141d696
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2026-03-12 10:49:08 +0600

    fix: menu fixed

--- data/menu-list.ts
+++ data/menu-list.ts
@@ -33,7 +33,7 @@ export function getMenuList(pathname: string): Group[] {
       groupLabel: "",
       menus: [
         {
-          href: "/",
+          href: "/dashboard",
           label: "Dashboard",
           icon: LayoutGrid,
           submenus: [],
