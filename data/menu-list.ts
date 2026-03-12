11bec14cd472533be148d3997204ee34c5cac0c9
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2026-03-12 10:41:46 +0600

    feat: changes completed

--- data/menu-list.ts
+++ data/menu-list.ts
@@ -33,7 +33,7 @@ export function getMenuList(pathname: string): Group[] {
       groupLabel: "",
       menus: [
         {
-          href: "/dashboard",
+          href: "/",
           label: "Dashboard",
           icon: LayoutGrid,
           submenus: [],
